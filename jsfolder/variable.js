240f524e2d0fee0bacbd2efed33f033e405694ea
Purnimak26 <142517209+Purnimak26@users.noreply.github.com>
2025-10-20 15:50:41 +0000

    added

---
+++ jsfolder/variable.js
@@ -0,0 +1,35 @@
+const accountId=1445;
+let accountEmail="purn@gmail.com";
+var accountPassword="457";
+accountCity="New Delhi";
+//accountId=4;
+//const change not allowed.can write but won't be updated.
+console.log(accountId);
+accountEmail="hs@gmail.com";
+console.log(accountEmail);
+accountCity="Bengaluru";
+console.log(accountCity);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+
+//console.table
+//used for printing several values together
+
+//const 
+//declared using const only
+
+//methods to declare vaiable
+//scope problem initially javascript didn't work with scope
+//never use var 
+//use const and let 
+/* prefer not to use var because of issue in block scope and functional scope
+*/
+
+//always declare the type first don't write directly
+
+let accountState;
+console.log(accountState);
+
+//if we just declare a variable and don't assign it a value then it's type is undefined
+
+
