984ca7afb819c86ec1b406369dceabd7d41dc67a
Purnimak26 <142517209+Purnimak26@users.noreply.github.com>
2025-10-24 15:18:00 +0000

    commit chane

---
+++ jsfolder/comparisonops.js
@@ -0,0 +1,86 @@
+/*console.log(2<8);
+console.log(4>7);
+//these are comaprison operators.problem occurs when we compare two values of different datatypes
+
+console.log("5">1);
+console.log("05">1);*/
+/*
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+
+console.log(undefined>=0);
+
+console.log(undefined<=0);
+
+//=== strict check
+//== check converts both of thrm to same dataype and then converts
+console.log("5">=5);
+console.log("5"===5);
+
+//primitive and non primitive data types
+//call by value,call by reference
+//primitive
+/*
+string
+number
+undefined
+null 
+symbol(used for making component unique)
+big int(bigger values)
+boolean
+
+examples
+const score=100;
+const temperature=50.7
+const name="Mumma"
+const speedoflight=3*10**8
+const isLoggedIn=true;
+
+*/
+//reference types
+/*
+array
+objects
+functions
+*/
+
+//declare symbols
+/*
+const id=Symbol('123');
+const anotherId=Symbol('123')
+console.log(id);
+console.log(anotherId);
+console.log(id===anotherId);
+
+
+/*is javascript dynamically typed?
+yes
+*/
+const score=100;
+//not defining data type here
+
+//reference type*/
+const flowers=['lily','daisy','rose','lotus'];
+let obj={
+    name:'niya',
+    age:56,
+};
+console.log(flowers);
+console.log(obj);
+console.log(obj.name);
+
+let sum=function(){
+    console.log(5+6);
+}
+    
+console.log(sum());
+console.log(typeof sum);
+
+
+
+
