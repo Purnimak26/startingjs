c1b4eb43d1c9537a8557ebbd81eaf93054a791ec
Purnimak26 <142517209+Purnimak26@users.noreply.github.com>
2025-10-25 06:46:30 +0000

    add

---
+++ jsfolder/numberandmaths.js
@@ -0,0 +1,30 @@
+/*const score=400;
+const balance=new Number(100);
+console.log(balance);
+console.log(balance.toString.length)
+
+const num2=67.89777;
+console.log(num2);
+console.log(num2.toString);
+console.log(num2.toPrecision(5))
+
+
+const hundreds=100000;
+console.log(hundreds.toLocaleString('en-IN'))
+*/
+//Maths
+console.log(Math)
+console.log(Math.abs(-90))
+console.log(Math.round(78.9087777,3));
+console.log(Math.floor(8.9))
+
+console.log((Math.random()*10)+1)
+//value between 0 and 1 only
+//whener want value between two number ex 30-40 multiply with 10
+const min=10
+const max=20
+//math.random return random value between 0 and 1 .in order to get value between two given points,we use (max-min+1)+min 
+console.log(Math.floor(Math.random()*(max-min+1)+(min)));
+//math.floor to get int value
+
+
