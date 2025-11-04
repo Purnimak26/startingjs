af62c1af8e8ece37e846b902171de5ac952b74f9
Purnimak26 <142517209+Purnimak26@users.noreply.github.com>
2025-11-04 09:13:06 +0000

    add it

---
+++ jsfolder/functions.js
@@ -0,0 +1,33 @@
+//Functions
+//packing code
+/*let name="World"
+function myname(){
+    console.log(`Hello,${name}`)
+}
+myname();*/
+//myname is just reference 
+//to call it myname();
+//no need to give type of numbers here
+/*function addTwonumbers(number1,number2){
+console.log(number1+number2)
+}
+addTwonumbers(4,6)
+addTwonumbers(7,'a')
+
+//parameters:placeholders in functions
+//arguments:actual values passed to the function
+addTwonumbers(4,null)
+const result=addTwonumbers(4,8)
+console.log(result)
+*/
+/*function sum(number1,number2){
+     =number1+number2;
+    return result;
+}
+console.log(sum(1,6));
+*/
+function loginUsername(username="sam"
+){
+    return `${username} just loggesd in`
+}
+console.log(loginUsername("Purnima"));
