c3c26add8a8c21eba96817d52b9161464168bb9e
Purnimak26 <142517209+Purnimak26@users.noreply.github.com>
2025-11-04 17:44:10 +0000

    add

---
+++ jsfolder/loops.js
@@ -0,0 +1,32 @@
+//iterators
+let sum=0;
+const arr=[2,5,3,3,5,2];
+/*for(let i=0;i<=10;i++){
+    const element=i;
+    if(element==5){
+        console.log("5 is the best number");
+    }
+    console.log(element);
+    
+}
+    */
+/*for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    
+}
+    */
+
+//while(condition)
+let i=0;
+while(i<=10){
+    console.log(i);
+    i+=2;
+}
+
+//do while 
+let score=1;
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score<5);
+   
