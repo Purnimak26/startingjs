07e03e6c2fc0a8c07d4265267cdcb0189363ea30
Purnimak26 <142517209+Purnimak26@users.noreply.github.com>
2025-11-06 16:43:45 +0000

    change

---
+++ jsfolder/map.js
@@ -0,0 +1,59 @@
+/* for of
+*/
+/*const arr=[2,3,4]
+for (const element of arr) {
+    console.log(element);
+}
+const greeting="Hello World";
+for(const element of greeting){
+    console.log(element)
+}
+//maps
+//holds key value pairs 
+const map=new Map()
+map.set('IN','India')
+map.set('USA',"United States of America")
+map.set('FR','France')
+console.log(map);
+for(const [key,value] of map){
+    console.log((key),':',value);
+}
+
+const myobject={
+    dish1:'noodles',
+    dish2:'momos'
+}
+
+const newObject={
+    js:'javacript',
+    cpp:'c++',
+    rb:'ruby',
+    sw:'swift'
+}
+for(const key in newObject){
+    console.log(key);
+    console.log(newObject[key]);
+    console.log(`${key} is shortcut for ${newObject[key]}`)
+}
+/* map 
+key:key
+value:object[key]
+
+*/
+
+/*const v=[1,2,3,8,7,9];
+for(const i in v){
+    console.log(v[i]);
+}
+    */
+let map=new Map();
+    map.set("1","one");
+    map.set("2","two");
+    map.set("3","three");
+
+/*for(const i in map){
+    console.log(i);
+}
+    */
+
+
